88b7c97d8bd4d9877e8b0d24dc68f03bf22f0330
yoooel <72613221+yoooel@users.noreply.github.com>
2021-06-22 00:44:57 +0000

    functionality started

---
+++ js/calculator.js
@@ -0,0 +1,4 @@
+// let caclculator = document.getElementsByClassName("calculator"); 
+let display = document.getElementById("display"); 
+let buttonValue = document.getElementsByTagName("button"); 
+console.log(buttonValue.value); 
