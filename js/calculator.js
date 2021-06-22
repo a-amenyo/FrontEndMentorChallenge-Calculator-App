4cd432aacc81f2b0b868bc9c67853086a5e2bd55
yoooel <72613221+yoooel@users.noreply.github.com>
2021-06-22 00:59:10 +0000

    values of buttons have been accessed

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,28 @@
 // let caclculator = document.getElementsByClassName("calculator"); 
-let display = document.getElementById("display"); 
-let buttonValue = document.getElementsByTagName("button"); 
-console.log(buttonValue.value); 
+let display = document.getElementById("display");
+// select all the buttons
+const buttons = document.querySelectorAll('button');
+// calculate function
+const calculate = (event)=> {
+    // current clicked buttons value
+    const clickedButtonValue = event.target.value;
+    console.log(clickedButtonValue); 
+  
+    if (clickedButtonValue === '=') {
+      // check if the display is not empty then only do the calculation
+      if (display.value !== '') {
+        // calculate and show the answer to display
+        display.value = eval(display.value);
+      }
+    } else if (clickedButtonValue === 'C') {
+      // clear everything on display
+      display.value = '';
+    } else {
+      // otherwise concatenate it to the display
+      display.value += clickedButtonValue;
+    }
+  }
+// add eventListener to each button
+buttons.forEach(function(button) {
+    button.addEventListener('click', calculate);
+  });
